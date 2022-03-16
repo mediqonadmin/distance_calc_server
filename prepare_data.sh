7b705e8c3d283e4b67701e1b3a84b4f45a720065
Hamidreza Seifi <hamidreza.seifi@mediqon.de>
2022-03-16 18:00:27 +0100

    add files

---
+++ prepare_data.sh
@@ -0,0 +1,161 @@
+#!/bin/sh
+
+echo "================================================================================================================================"
+echo "Install git if is not installed"
+
+sudo apt update
+sudo apt install git
+
+ROOT_DIR="/home/admin/osrm"
+
+if [ -d "$ROOT_DIR" ]; then
+  # Take action if $ROOT_DIR exists. #
+  rm -rf "$ROOT_DIR"
+  echo "Deleting ${ROOT_DIR}..."
+else
+  echo "Folder ${ROOT_DIR} does not exists"
+fi
+
+mkdir osrm
+cd osrm
+
+echo "================================================================================================================================"
+echo "Cloning osrm-backend"
+
+git clone -b v5.26.0 https://github.com/Project-OSRM/osrm-backend.git 
+
+echo "================================================================================================================================"
+echo "Download and extract osrm binaries"
+
+wget https://github.com/Project-OSRM/osrm-backend/releases/download/v5.26.0/node_osrm-v5.26.0-node-v64-linux-x64-Release.tar.gz
+
+tar xvzf node_osrm-v5.26.0-node-v64-linux-x64-Release.tar.gz
+
+echo "================================================================================================================================"
+echo "Copy profiles from osrm-backend  and libstdc++.so.6 to binding"
+
+cp -r  osrm-backend/profiles/ binding/
+
+cp /usr/lib/x86_64-linux-gnu/libstdc++.so.6 binding/
+
+echo "================================================================================================================================"
+echo "Removing osrm-backend and binaries tar.gz package"
+
+rm -rf osrm-backend/
+
+rm  node_osrm-v5.26.0-node-v64-linux-x64-Release.tar.gz
+
+echo "================================================================================================================================"
+echo "Downloading osmconvert64"
+
+cd binding
+
+wget https://github.com/ITSLeeds/OSMtools/raw/master/inst/osmconvert64
+
+sudo chmod 777 osmconvert64
+
+echo "================================================================================================================================"
+echo "Downloading geo files ..."
+
+mkdir pre_data
+
+cd pre_data
+
+wget http://download.geofabrik.de/europe/france/alsace-latest.osm.pbf
+
+wget http://download.geofabrik.de/europe/austria-latest.osm.pbf
+
+wget http://download.geofabrik.de/europe/belgium-latest.osm.pbf
+
+wget http://download.geofabrik.de/europe/czech-republic-latest.osm.pbf
+
+wget http://download.geofabrik.de/europe/denmark-latest.osm.pbf
+
+wget http://download.geofabrik.de/europe/poland/dolnoslaskie-latest.osm.pbf
+
+wget http://download.geofabrik.de/europe/netherlands/drenthe-latest.osm.pbf
+
+wget http://download.geofabrik.de/europe/netherlands/friesland-latest.osm.pbf
+
+wget http://download.geofabrik.de/europe/netherlands/gelderland-latest.osm.pbf
+
+wget http://download.geofabrik.de/europe/germany-latest.osm.pbf
+
+wget http://download.geofabrik.de/europe/netherlands/groningen-latest.osm.pbf
+
+wget http://download.geofabrik.de/europe/netherlands/limburg-latest.osm.pbf
+
+wget http://download.geofabrik.de/europe/france/lorraine-latest.osm.pbf
+
+wget http://download.geofabrik.de/europe/poland/lubuskie-latest.osm.pbf
+
+wget http://download.geofabrik.de/europe/luxembourg-latest.osm.pbf
+
+wget http://download.geofabrik.de/europe/netherlands/overijssel-latest.osm.pbf
+
+wget http://download.geofabrik.de/europe/switzerland-latest.osm.pbf
+
+wget http://download.geofabrik.de/europe/poland/zachodniopomorskie-latest.osm.pbf
+
+
+echo "================================================================================================================================"
+echo "Downloading geo files is done."
+echo "================================================================================================================================"
+echo "Converting geo files .."
+
+cd ..
+mkdir converted_data
+
+./osmconvert64  ./pre_data/alsace-latest.osm.pbf -o=./converted_data/alsace.o5m
+./osmconvert64  ./pre_data/austria-latest.osm.pbf -o=./converted_data/austria.o5m
+./osmconvert64  ./pre_data/belgium-latest.osm.pbf -o=./converted_data/belgium.o5m
+./osmconvert64  ./pre_data/czech-republic-latest.osm.pbf -o=./converted_data/czech_republic.o5m
+./osmconvert64  ./pre_data/denmark-latest.osm.pbf -o=./converted_data/denmark.o5m
+./osmconvert64  ./pre_data/dolnoslaskie-latest.osm.pbf -o=./converted_data/dolnoslaskie.o5m
+./osmconvert64  ./pre_data/drenthe-latest.osm.pbf -o=./converted_data/drenthe.o5m
+./osmconvert64  ./pre_data/friesland-latest.osm.pbf -o=./converted_data/friesland.o5m
+./osmconvert64  ./pre_data/gelderland-latest.osm.pbf -o=./converted_data/gelderland.o5m
+./osmconvert64  ./pre_data/germany-latest.osm.pbf -o=./converted_data/germany.o5m
+./osmconvert64  ./pre_data/groningen-latest.osm.pbf -o=./converted_data/groningen.o5m
+./osmconvert64  ./pre_data/limburg-latest.osm.pbf -o=./converted_data/limburg.o5m
+./osmconvert64  ./pre_data/lorraine-latest.osm.pbf -o=./converted_data/lorraine.o5m
+./osmconvert64  ./pre_data/lubuskie-latest.osm.pbf -o=./converted_data/lubuskie.o5m
+./osmconvert64  ./pre_data/luxembourg-latest.osm.pbf -o=./converted_data/luxembourg.o5m
+./osmconvert64  ./pre_data/overijssel-latest.osm.pbf -o=./converted_data/overijssel.o5m
+./osmconvert64  ./pre_data/switzerland-latest.osm.pbf -o=./converted_data/switerland.o5m
+./osmconvert64  ./pre_data/zachodniopomorskie-latest.osm.pbf -o=./converted_data/zachodniopomorskie.o5m
+
+echo "================================================================================================================================"
+echo "Converting geo files is done."
+
+echo "================================================================================================================================"
+echo "Merging geo files in germany_eu.osm.pbf ..."
+
+
+mkdir all_data
+
+./osmconvert64 ./converted_data/alsace.o5m ./converted_data/austria.o5m ./converted_data/belgium.o5m ./converted_data/czech_republic.o5m ./converted_data/denmark.o5m ./converted_data/dolnoslaskie.o5m ./converted_data/drenthe.o5m ./converted_data/friesland.o5m ./converted_data/gelderland.o5m ./converted_data/germany.o5m ./converted_data/groningen.o5m ./converted_data/limburg.o5m ./converted_data/lorraine.o5m ./converted_data/lubuskie.o5m ./converted_data/luxembourg.o5m ./converted_data/overijssel.o5m ./converted_data/switerland.o5m ./converted_data/zachodniopomorskie.o5m -o=./all_data/germany_eu.osm.pbf
+
+echo "================================================================================================================================"
+echo "Merging geo files in germany_eu.osm.pbf is done."
+
+
+echo "================================================================================================================================"
+echo "Delete geo part files ..."
+
+rm -rf pre_data
+rm -rf converted_data
+
+echo "================================================================================================================================"
+echo "Delete geo part files done."
+
+echo "================================================================================================================================"
+echo "Preparing osrm files ..."
+
+./osrm-extract ./all_data/germany_eu.osm.pbf -p ./profiles/car.lua
+
+./osrm-contract ./all_data/germany_eu.osm.pbf
+
+echo "================================================================================================================================"
+echo "Preparing osrm files done."
+
