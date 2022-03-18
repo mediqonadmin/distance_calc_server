0a7cbe9074f022c6bcb65c02189a8295e0eba63d
Hamidreza Seifi <hamidreza.seifi@mediqon.de>
2022-03-18 12:28:15 +0100

    add files

--- start.sh
+++ start.sh
@@ -4,6 +4,8 @@
 sudo apt update
 sudo apt-get install p7zip
 
+sudo sudo apt install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev curl libbz2-dev
+
 rm -rf osrm
 
 mkdir osrm
@@ -12,8 +14,9 @@ cd osrm
 
 wget https://github.com/mediqonadmin/distance_calc_server/raw/main/distance_calc_server.7z
 
-tar xvzf distance_calc_server.tar.gz
+7zr x distance_calc_server.7z
 
-rm distance_calc_server.tar.gz
+rm distance_calc_server.7z
 
-./prepare_data.sh
+sudo chmod 777 *.sh
+
